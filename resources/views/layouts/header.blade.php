4e8fb0c360a6f7dfc507c3796734f0994b595189
Tales Cardoso <105746645+talesbruno@users.noreply.github.com>
2022-05-25 15:20:00 -0300

    Iniciando bootstrap

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,62 +2,79 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('titulo')</title>
+  <title>@yield('titulo')</title>
 
-    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="/css/styles.css">
 
-    
+  <link rel="stylesheet" href="/css/styles.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/fbc04c1fa8.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
 </head>
 
 <body>
-    <header>
-        <nav>
-            <div class="logo">
-                <img src="./img/logo.png">
-            </div>
-            <div class="mobile-menu">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
-            </div>
-            <ul class="nav-list">
-                <li><a href="/">Inicio</a></li>
-                <li><a href="/">Sobre</a></li>
-                <li><a href="/Pontos_Turisticos/listar">Pontos Turísticos</a></li>
-                <li><a href="/">Hospedagem</a></li>
-                <li><a href="/">Comer e Beber</a></li>
-                @guest
-                <li><a href="/login">Entrar</a></li>
-                @endguest
-                @auth
-                <li><a href="/dashboard">Meus Locais</a></li>
-                <li><form action="/logout" method="POST">
-                    @csrf
-                    <a href="/logout" onclick="event.preventDefault();
+  <header>
+    <div class="container" id="nav-container">
+      <nav class="navbar navbar-expand-lg fixed-top navbar-dark">
+        <a class="navbar-brand" href="index.html">
+          <img id="logo" src="./img/logo.png" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links" aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link" href="/">Inicio</a>
+            <a class="nav-item nav-link" href="/">Sobre</a>
+            <a class="nav-item nav-link" href="/Pontos_Turisticos/listar">Pontos Turísticos</a>
+            <a class="nav-item nav-link" href="/">Hospedagem</a>
+            <a class="nav-item nav-link" href="/">Comer e Beber</a>
+            @guest
+            <a class="nav-item nav-link" href="/login">Entrar</a>
+            @endguest
+            @auth
+            <a class="nav-item nav-link" href="/dashboard">Meus Locais</a>
+            <form action="/logout" method="POST">
+              @csrf
+              <a class="nav-item nav-link" href="/logout" onclick="event.preventDefault();
                     this.closest('form').submit();">Sair</a>
-                </form></li>
-                @endauth
-            </ul>
-        </nav>
-    </header>
-    
-    <main>
-        <div>
-            @if(session('msg'))
-                <p>{{ session('msg') }}</p>
-                @endif
-        @yield('conteudo')
+            </form>
+            @endauth
+          </div>
         </div>
-      
- </main>
-    <footer>
-        <p>Tales e Matheus &copy; 2022</p>
-    </footer>
+      </nav>
+    </div>
+  </header>
+
+
+  <main>
+
+    <div>
+      @if(session('msg'))
+      <p>{{ session('msg') }}</p>
+      @endif
+      @yield('conteudo')
+    </div>
+
+  </main>
+
+  <footer>
+    <div class="fixed-bottom">
+      <div id="copy-area">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <p>Tales e Matheus &copy; 2022</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </body>
 
 </html>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,114 +8,89 @@
     <title>@yield('titulo')</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
 
     <link rel="stylesheet" href="/css/styles.css">
-
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/fbc04c1fa8.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
 </head>
 
 <body>
     <header>
-        <nav>
-            <div class="logo">
-                <img src="./img/logo.png">
-            </div>
-            <div class="mobile-menu">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
-            </div>
-            <ul class="nav-list">
-                <li><a href="/">Inicio</a></li>
-                <li><a href="/">Sobre</a></li>
-                <li><a href="/Pontos_Turisticos/listar">Pontos Turísticos</a></li>
-                <li><a href="/">Hospedagem</a></li>
-                <li><a href="/">Comer e Beber</a></li>
-                @guest
-                <li><a href="/login">Entrar</a></li>
-                @endguest
-                @auth
-                <li><a href="/dashboard">Meus Locais</a></li>
-                <li><form action="/logout" method="POST">
-                    @csrf
-                    <a href="/logout" onclick="event.preventDefault();
+        <div class="container" id="nav-container">
+            <nav class="navbar navbar-expand-lg fixed-top navbar-dark">
+                <a class="navbar-brand" href="index.html">
+                    <img id="logo" src="./img/logo.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links" aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link" href="/">Inicio</a>
+                        <a class="nav-item nav-link" href="/">Sobre</a>
+                        <a class="nav-item nav-link" href="/Pontos_Turisticos/listar">Pontos Turísticos</a>
+                        <a class="nav-item nav-link" href="/">Hospedagem</a>
+                        <a class="nav-item nav-link" href="/">Comer e Beber</a>
+                        @guest
+                        <a class="nav-item nav-link" href="/login">Entrar</a>
+                        @endguest
+                        @auth
+                        <a class="nav-item nav-link" href="/dashboard">Meus Locais</a>
+                        <form action="/logout" method="POST">
+                            @csrf
+                            <a class="nav-item nav-link" href="/logout" onclick="event.preventDefault();
                     this.closest('form').submit();">Sair</a>
-                </form></li>
-                @endauth
-            </ul>
-        </nav>
+                        </form>
+                        @endauth
+                    </div>
+                </div>
+            </nav>
+        </div>
     </header>
-    
+
     <main>
         <div>
             @if(session('msg'))
-                <p>{{ session('msg') }}</p>
-                @endif
-        
+            <p>{{ session('msg') }}</p>
+            @endif
+
         </div>
-      <section class="textoprincipal">
 
-      <div class="slider">
-      <div class="slides">    
-       
-         <input type="radio" name="radio-btn" id="radio1">
-         <input type="radio" name="radio-btn" id="radio2">
-         <input type="radio" name="radio-btn" id="radio3">
-         <input type="radio" name="radio-btn" id="radio4">
-        
 
-           
-         <div class="slide first">
-             <img src="./img/1.png" alt="imagem 1"/>
-         </div>
-         <div class="slide">
-             <img src="./img/2.png" alt="imagem 2"/>
-         </div>
-         <div class="slide">
-             <img src="./img/3.png" alt="imagem 3"/>
-         </div>
-         <div class="slide">
-             <img src="./img/4.png" alt="imagem 4"/>
-         </div>
-        
+        <div class="titulos">
+            <h1>Bem-vindo a</h1>
+            <h1>PORTEIRINHA</h1>
+            <div class="barra">
+                <form action="/" method="GET">
+                    <input type="text" id="pesquisa" name="pesquisa" class="bar" placeholder="O que você está procurando?">
+                    <button type="submit" class="botaobar">
+                </form>
 
-        
-       <div class="navigation-auto">
-           <div class="auto-btn1"></div>
-           <div class="auto-btn2"></div>
-           <div class="auto-btn3"></div>
-           <div class="auto-btn4"></div>
-       </div>
-        
-    </div>
+                <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </div>
 
-      <div class="manual-navigation">
-          <label for="radio1" class="manual-btn"></label>
-          <label for="radio2" class="manual-btn"></label>
-          <label for="radio3" class="manual-btn"></label>
-          <label for="radio4" class="manual-btn"></label>
-      </div>  
-    </div>
-        
-      <div class="titulos">
-      <h1 >Bem-vindo a</h1>
-      <h1 >PORTEIRINHA</h1>
-        <div class="barra">
-            <form action="/" method="GET">
-            <input type="text" id="pesquisa" name="pesquisa" class="bar" placeholder="O que você está procurando?">
-            <button type="submit" class="botaobar">
-            </form>
-      
-        <i class="fa fa-search"></i>
-     </button>
-     </div>
-   </div>
-   
-   </section>
- </main>
- @yield('conteudo')
+        </section>
+    </main>
+    @yield('conteudo')
     <footer>
-        <p>Tales e Matheus &copy; 2022</p>
+        <div class="fixed-bottom">
+            <div id="copy-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p>Tales e Matheus &copy; 2022</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </footer>
     <script src="{{ asset('js/script.js') }}"></script>
     <script src="{{ asset('js/script2.js') }}"></script>
